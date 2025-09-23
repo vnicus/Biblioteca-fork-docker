a01248ea43b08923033a97494c7d9f4a41ed6a95
vnicus <email@email.com>
2025-09-23 12:35:06 -0300

    att confing

--- App/config.php
+++ App/config.php
@@ -3,7 +3,7 @@
 define('BASE_DIR', dirname(__FILE__, 2));
 define('VIEWS', BASE_DIR . '/App/View');
 
-$_ENV['db']['host'] = "localhost:3307";
+$_ENV['db']['host'] = "localhost:3306";
 $_ENV['db']['user'] = "root";
-$_ENV['db']['pass'] = "etecjau";
-$_ENV['db']['database'] = "biblioteca";
+$_ENV['db']['pass'] = "root";
+$_ENV['db']['database'] = "db_biblioteca";
